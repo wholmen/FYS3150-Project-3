944fbf064342d54d8302cdeab531b064f88364e4
wholmen <fwholmen@gmail.com>
2013-10-17 00:23:00 +0200

    Starting to implement RK4. Deleted almost all previous work. Now I consider coordinates as a 2d array.

--- SolarSystem/main.cpp
+++ SolarSystem/main.cpp
@@ -3,12 +3,14 @@
 #include <armadillo>
 #include <cmath>
 #include <math.h>
+#include <fstream>
 
 using namespace std;
 using namespace arma;
 
-void RK4(double *x, double *y, double *vx, double *vy, double T, double dt, int N);
-double Force(double x, double y,int o);
+void RK4(vec *r, vec *v, vec dist, double m, double dt);
+vec inline f(vec r, double m);
+vec inline DIST(vec r1, vec r2);
 
 /*
  1. Constants
@@ -28,43 +30,45 @@ double Force(double x, double y,int o);
 int main()
 {
     double Tfinal = 40; //years
-    double delta_t = 0.1; //years
+    double delta_t = 0.001; //years
     int N = Tfinal/delta_t;
 
     // Initializing earth-sun
     vec X = zeros<vec>(N); vec Vx = zeros<vec>(N);
     vec Y = zeros<vec>(N); vec Vy = zeros<vec>(N);
-    X(0) = Rearth;   Vx(0) = 0; // Initial conditions for x-position and velocity
+    X(0) = Rearth * AU;   Vx(0) = 0; // Initial conditions for x-position and velocity
     Y(0) = 0;        Vy(0) = sqrt(Gconst * Msun) / (X(0)*X(0) + Y(0)*Y(0)); // Initial conditions for y-position and vecolity
 
-    RK4(&X(0),&Y(0),&Vx(0),&Vy(0),Tfinal,delta_t,N);
-
-
-}
-
-void RK4(double *x, double *y, double *vx, double *vy, double T, double dt, int N){
-
-
-    // Euler-Chromer
-    double ax,ay,Fx,Fy;
-    for (int i=0; i<N; i++){
-        Fx = Force(x[i],y[i],0);
-        Fy = Force(x[i],y[i],1);
-        ax = Fx/Mearth;
-        ay = Fy/Mearth;
-        vx[i+1] = vx[i] + ax*dt;
-        vy[i+1] = vy[i] + ay*dt;
-        x[i+1] = x[i] + vx[i+1]*dt;
-        y[i+1] = y[i] + vy[i+1]*dt;
+    for (int i=0;i<N;i++){
+        vec r = zeros<vec>(2); vec v = zeros<vec>(2);
+        r << X(i) << Y(i); v << Vx(i) << Vy(i);
+        RK4(&r,&v,r,Msun,delta_t);
+        X(i+1) = r(0); Y(i+1) = r(1); Vx(i+1) = v(0); Vy(i+1) = v(1);
     }
+
+
+
+    ofstream myfile;
+    myfile.open("earth_sun.txt");
+    for (int i=0;i<N;i++){
+        myfile << X(i) << " " << Y(i) << " " << i*delta_t << endl;
+    }
+    myfile.close();
+
 }
 
-double Force(double x, double y, int o){
-    double F;
-    if (o==0){ //x-direction
-        F = -Gconst * Mearth * Msun / (x*x*x + y*y*y) * x;
-    }else if (o==1){ //y-direction
-        F = -Gconst * Mearth * Msun / (x*x*x + y*y*y) * y;
-    }
-    return F;
+void RK4(vec *r, vec *v, vec dist, double m, double dt){
+    vec k1 = zeros<vec>(2); vec k2 = zeros<vec>(2); vec k3 = zeros<vec>(2); vec k4 = zeros<vec>(2);
+    k1 = dt * f(R, m);
+    k2 = dt * f(R + k1/2, m);
+    k3 = dt * f(R + k2/2, m);
+    k4 = dt * f(R + k3, m);
+    v = v + 1/6 * (k1 + 2*k2 + 2*k3 + k4);
+    r = v * dt;
 }
+
+vec inline f(vec r, double m){ // r is the position [x,y]. m is not the moving planets mass, but the interracting object.
+    Gconst * m / sqrt(r(0)*r(0)*r(0) + r(1)*r(1)*r(1)) * r;
+}
+
+vec inline DIST(vec r1, vec r2){ return r2 - r1; } // Returning distance between two planets
